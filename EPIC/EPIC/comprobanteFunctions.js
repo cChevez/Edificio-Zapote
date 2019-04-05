73f65b0dbec7b2419672fbc4c2b44db12983512d
Carlos Chevez <47582635+cChevez@users.noreply.github.com>
2019-04-05 16:03:32 -0600

    errores

--- EPIC/EPIC/comprobanteFunctions.js
+++ EPIC/EPIC/comprobanteFunctions.js
@@ -19,29 +19,3 @@ function solicitudFactura() {
         dato0.style.display = "none";
     }
 }
-
-var myform = $("form#facturaElec");
-myform.submit(function (event) {
-    event.preventDefault();
-
-    var params = myform.serializeArray().reduce(function (obj, item) {
-        obj[item.name] = item.value;
-        return obj;
-    }, {});
-
-    // Change to your service ID, or keep using the default service
-    var service_id = "default_service";
-
-    var template_id = "facturaelectronica";
-    myform.find("button").text("Enviando...");
-    emailjs.send(service_id, template_id, params)
-        .then(function () {
-            alert("Enviado!");
-            myform.find("button").text("Send");
-        }, function (err) {
-            alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-            myform.find("button").text("Send");
-        });
-
-    return false;
-});
