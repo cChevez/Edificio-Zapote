baadb6cdcf78f203035f943ed89c65ea1783f017
andreduard <andreguti333@gmail.com>
2019-05-12 21:12:43 -0600

    Arreglo del envio de msj de la reservacion

--- DB/procedures.sql
+++ DB/procedures.sql
@@ -14,6 +14,110 @@ go
 --);
 --go
 
+create or alter procedure NotificarReservacion @fkid int
+as begin
+	DECLARE @id int 
+	declare @Email varchar(max)
+	DECLARE @dia varchar(20)
+	DECLARE @horaInicio varchar(20)
+	DECLARE @horaFinal varchar(20)
+	DECLARE @Lab int
+	DECLARE @aula int
+	DECLARE @MensajeReservacion varchar(max)
+	SET @id = @@IDENTITY
+	DECLARE @contador int
+	SET @contador=1
+
+	IF OBJECT_ID('tempdb..#temp') IS NOT NULL
+	DROP TABLE #temp
+
+	PRINT @id
+
+	Select @Email = (select distinct R.email
+	from Reservacion R 
+	inner join HorarioReservado HR on HR.FKReservacion = R.id
+	where R.id = @fkid)
+
+	select distinct R.id, R.fechaSolicitud, R.nombreSolicitante, R.nombreEmpresa, R.cedulaJuridica, R.email, 
+	R.numeroTelefono, R.nombreActividad, R.fechaInicioActividad, R.fechaFinalActividad
+	from Reservacion R 
+	inner join HorarioReservado HR on HR.FKReservacion = R.id
+	where R.id = @fkid
+
+	--select HR.id, HR.dia, HR.horaInicio, HR.horaFinal, HR.FKAula, HR.FKLaboratorio
+	--from HorarioReservado HR where HR.FKReservacion = 1
+
+	SELECT HR.dia, HR.horaInicio, HR.horaFinal, HR.FKAula, HR.FKLaboratorio ,IDENTITY(INT,1,1) AS rn INTO #temp 	
+	from HorarioReservado HR where HR.FKReservacion = @fkid
+
+	Select * from #temp
+
+	DECLARE @MensajeHoras varchar(max)
+	DECLARE @MensajeHoras2 varchar(max)
+	SET @MensajeHoras2 = ''
+
+	SELECT @MensajeHoras = CHAR(09) +'Dia' +CHAR(09) + CHAR(09) + CHAR(124) +CHAR(09) + 'Hora Inicio' + CHAR(08) +CHAR(09) + CHAR(09) + CHAR(124) +CHAR(09) + 'Hora Final' + CHAR(09) + CHAR(09) + CHAR(124) +CHAR(09) + 'Aula' +  CHAR(09) + CHAR(124) +CHAR(09) + 'Laboratorio' + CHAR(10)
+	--Print @MensajeHoras
+
+	while @contador <= (SELECT COUNT(*) FROM #temp)
+	BEGIN
+	SELECT @dia=convert(varchar, dia, 105), @horaInicio=convert(varchar, horaInicio, 8), @horaFinal=convert(varchar, horaFinal, 8), @aula=FKAula, @Lab=FKLaboratorio FROM #temp WHERE rn=@contador
+	
+	if @aula is not null
+		begin
+			SELECT @MensajeHoras += @dia + CHAR(09) + CHAR(09) + @horaInicio +CHAR(09) +CHAR(09) + CHAR(09) + @horaFinal +CHAR(09) +CHAR(09) + CHAR(09) + CAST(@aula as varchar(10)) +CHAR(09) +CHAR(09) +CHAR(09) + '' + CHAR(10)
+		end
+	else
+		begin 
+			SELECT @MensajeHoras += @dia + CHAR(09) + CHAR(09) + @horaInicio + CHAR(09) +CHAR(09) +CHAR(09) + @horaFinal +CHAR(09) +CHAR(09) + CHAR(09) + '' +CHAR(09) +CHAR(09) +CHAR(09) + CAST(@Lab as varchar(10)) + CHAR(10)
+		end
+
+	--SELECT @MensajeHoras2 = '05-12-19'  +CHAR(09) + CHAR(124) +CHAR(09) + '12:00'  +CHAR(09) +CHAR(09) + CHAR(124) +CHAR(09) + '15:00' +CHAR(09) +CHAR(09) + CHAR(124) +CHAR(09) + CAST(@aula as varchar(10)) +CHAR(09) +CHAR(09) + CHAR(124) +CHAR(09)  + '2' + CHAR(10)
+
+	--Print @MensajeHoras2
+
+	SET @contador=@contador+1
+
+	END
+
+	Select @MensajeReservacion = (select distinct 'Se ha creado su reservación exitosamente.' + CHAR(10) + CHAR(13) + CHAR(10) + CHAR(13) + 
+	'Datos de Reservacion' + CHAR(10) + CHAR(13) + CHAR(10) + CHAR(13) + 
+	'Numero de Reservacion: '+ CAST(R.id as varchar(10))  + CHAR(10) + CHAR(13) +
+	'Fecha de creación: ' + CAST(R.fechaSolicitud as varchar(10)) + CHAR(10) + CHAR(13) +
+	'Nombre del reservante: ' + R.nombreSolicitante + CHAR(10) + CHAR(13) +
+	'Nombre de la empresa: ' + R.nombreEmpresa + CHAR(10) + CHAR(13) +
+	'Cédula jurídica: ' + R.cedulaJuridica + CHAR(10) + CHAR(13) +
+	'Correo electrónico: ' + R.email + CHAR(10) + CHAR(13) +
+	'Teléfono: ' + R.numeroTelefono + CHAR(10) + CHAR(13) +
+	'Nombre de la actividad: ' + R.nombreActividad + CHAR(10) + CHAR(13) +
+	'Fecha de inicio: ' + CAST(R.fechaInicioActividad as varchar(10)) + CHAR(10) + CHAR(13) +
+	'Fecha de finalización: ' + CAST(R.fechaFinalActividad as varchar(10)) + CHAR(10) + CHAR(13)+
+	'Horario reservado: ' + CHAR(10) + CHAR(13) + CHAR(10) + CHAR(13)
+	from Reservacion R 
+	inner join HorarioReservado HR on HR.FKReservacion = R.id
+	where R.id = @fkid)
+
+	PRINT @MensajeReservacion
+	PRINT @MensajeHoras
+	--Print @MensajeHoras2
+
+	declare @titulo varchar(max)
+	Select @titulo = 'Notificacion Reservacion'
+	declare @mensaje varchar(max)
+	SET @mensaje = @MensajeReservacion + @MensajeHoras
+	
+	EXEC msdb.dbo.sp_send_dbmail
+		@profile_Name = 'ProyectoEmail',
+		--@recipients = @copy_to,
+		@recipients = @Email,
+		@subject = @titulo,
+		@body = @Mensaje
+		--@body_format = 'HTML'
+end 
+
+go
+
+
 create or alter procedure insertarReservacion @fechaSolicitud date, @nombreSolicitante nvarchar(50), @empresaSolicitante nvarchar(50), @nombreActividad nvarchar(50), @cedulaJuridica varchar(50), @email nvarchar(50), @numeroTelefono varchar(50),
 @descripcion nvarchar(100), @cantidadParticipantes int, 
 --@dia date, @horaInicio datetime, @horaFinal datetime, 
@@ -51,6 +155,7 @@ as begin
 		insert into Reservacion(fechaSolicitud, nombreSolicitante, nombreEmpresa, nombreActividad, cedulaJuridica, email, numeroTelefono, descripcion, cantidadParticipantes, videoBin, FKTipoReservacion, FKAdministrador, FKEstadoReservacion,fechaInicioActividad,fechaFinalActividad,monto) values (@fechaSolicitud, @nombreSolicitante, @empresaSolicitante,@nombreActividad, @cedulaJuridica, @email, @numeroTelefono, @descripcion,@cantidadParticipantes, @videobin, @TipoReservacion, @administrador, 1,@fechaInicioActividad,@FechaFinalActividad,@monto)
 		declare @FKid int 
 		set @FKid = @@IDENTITY
+		PRINT @FKid
 		insert into HorarioReservado(dia, horaInicio, horaFinal, FKAula, FKLaboratorio,FKReservacion) 
 			select T.dia, T.horaInicio, T.horaFinal, A.id, null, @FKid
 			from HorasSolicitudTable T
@@ -62,9 +167,7 @@ as begin
 			inner join Laboratorio L on L.numeroLab = T.numLab
 			where T.numAula is null
 		select * from HorarioReservado
-			Exec NotificarReservacion @FKid
-
-		
+		Exec NotificarReservacion @FKid
 	commit
 	return @@identity
 	end try
@@ -75,7 +178,6 @@ as begin
 	end catch
 end
 go
-
 
 create or alter procedure insertarTotal @fechaReservacion date, @nombre varchar(50), @empresa varchar(50), @cedula varchar(50) , @correo varchar(50), @telefono varchar(50), 
 @nombreActividad varchar(50), @fechaInicio date, @fechaFinal date, @observaciones varchar(250), @cantidadParticipantes int, @monto int
@@ -270,107 +372,6 @@ as begin
 end
 go
 
-create or alter procedure NotificarReservacion @fkid int
-as begin
-	DECLARE @id int 
-	declare @Email varchar(max)
-	DECLARE @dia varchar(20)
-	DECLARE @horaInicio varchar(20)
-	DECLARE @horaFinal varchar(20)
-	DECLARE @Lab int
-	DECLARE @aula int
-	DECLARE @MensajeReservacion varchar(max)
-	SET @id = @@IDENTITY
-	DECLARE @contador int
-	SET @contador=1
-
-	IF OBJECT_ID('tempdb..#temp') IS NOT NULL
-	DROP TABLE #temp
-
-	PRINT @id
-
-	Select @Email = (select distinct R.email
-	from Reservacion R 
-	inner join HorarioReservado HR on HR.FKReservacion = R.id)
-
-	select distinct R.id, R.fechaSolicitud, R.nombreSolicitante, R.nombreEmpresa, R.cedulaJuridica, R.email, 
-	R.numeroTelefono, R.nombreActividad, R.fechaInicioActividad, R.fechaFinalActividad
-	from Reservacion R 
-	inner join HorarioReservado HR on HR.FKReservacion = R.id
-
-	--select HR.id, HR.dia, HR.horaInicio, HR.horaFinal, HR.FKAula, HR.FKLaboratorio
-	--from HorarioReservado HR where HR.FKReservacion = 1
-
-	SELECT HR.dia, HR.horaInicio, HR.horaFinal, HR.FKAula, HR.FKLaboratorio ,IDENTITY(INT,1,1) AS rn INTO #temp 	
-	from HorarioReservado HR where HR.FKReservacion = @fkid
-
-	Select * from #temp
-
-	DECLARE @MensajeHoras varchar(max)
-	DECLARE @MensajeHoras2 varchar(max)
-	SET @MensajeHoras2 = ''
-
-	SELECT @MensajeHoras = CHAR(09) +'Dia' +CHAR(09) + CHAR(09) + CHAR(124) +CHAR(09) + 'Hora Inicio' + CHAR(08) +CHAR(09) + CHAR(09) + CHAR(124) +CHAR(09) + 'Hora Final' + CHAR(09) + CHAR(09) + CHAR(124) +CHAR(09) + 'Aula' +  CHAR(09) + CHAR(124) +CHAR(09) + 'Laboratorio' + CHAR(10)
-	--Print @MensajeHoras
-
-	while @contador <= (SELECT COUNT(*) FROM #temp)
-	BEGIN
-	SELECT @dia=convert(varchar, dia, 105), @horaInicio=convert(varchar, horaInicio, 8), @horaFinal=convert(varchar, horaFinal, 8), @aula=FKAula, @Lab=FKLaboratorio FROM #temp WHERE rn=@contador
-	
-	if @aula is not null
-		begin
-			SELECT @MensajeHoras += @dia + CHAR(09) + CHAR(09) + @horaInicio +CHAR(09) +CHAR(09) + CHAR(09) + @horaFinal +CHAR(09) +CHAR(09) + CHAR(09) + CAST(@aula as varchar(10)) +CHAR(09) +CHAR(09) +CHAR(09) + '' + CHAR(10)
-		end
-	else
-		begin 
-			SELECT @MensajeHoras += @dia + CHAR(09) + CHAR(09) + @horaInicio + CHAR(09) +CHAR(09) +CHAR(09) + @horaFinal +CHAR(09) +CHAR(09) + CHAR(09) + '' +CHAR(09) +CHAR(09) +CHAR(09) + CAST(@Lab as varchar(10)) + CHAR(10)
-		end
-
-	--SELECT @MensajeHoras2 = '05-12-19'  +CHAR(09) + CHAR(124) +CHAR(09) + '12:00'  +CHAR(09) +CHAR(09) + CHAR(124) +CHAR(09) + '15:00' +CHAR(09) +CHAR(09) + CHAR(124) +CHAR(09) + CAST(@aula as varchar(10)) +CHAR(09) +CHAR(09) + CHAR(124) +CHAR(09)  + '2' + CHAR(10)
-
-	--Print @MensajeHoras2
-
-	SET @contador=@contador+1
-
-	END
-
-	Select @MensajeReservacion = (select distinct 'Se ha creado su reservación exitosamente.' + CHAR(10) + CHAR(13) + CHAR(10) + CHAR(13) + 
-	'Datos de Reservacion' + CHAR(10) + CHAR(13) + CHAR(10) + CHAR(13) + 
-	'Numero de Reservacion: '+ CAST(R.id as varchar(10))  + CHAR(10) + CHAR(13) +
-	'Fecha de creación: ' + CAST(R.fechaSolicitud as varchar(10)) + CHAR(10) + CHAR(13) +
-	'Nombre del reservante: ' + R.nombreSolicitante + CHAR(10) + CHAR(13) +
-	'Nombre de la empresa: ' + R.nombreEmpresa + CHAR(10) + CHAR(13) +
-	'Cédula jurídica: ' + R.cedulaJuridica + CHAR(10) + CHAR(13) +
-	'Correo electrónico: ' + R.email + CHAR(10) + CHAR(13) +
-	'Teléfono: ' + R.numeroTelefono + CHAR(10) + CHAR(13) +
-	'Nombre de la actividad: ' + R.nombreActividad + CHAR(10) + CHAR(13) +
-	'Fecha de inicio: ' + CAST(R.fechaInicioActividad as varchar(10)) + CHAR(10) + CHAR(13) +
-	'Fecha de finalización: ' + CAST(R.fechaFinalActividad as varchar(10)) + CHAR(10) + CHAR(13)+
-	'Horario reservado: ' + CHAR(10) + CHAR(13) + CHAR(10) + CHAR(13)
-	from Reservacion R 
-	inner join HorarioReservado HR on HR.FKReservacion = R.id )
-
-	PRINT @MensajeReservacion
-	PRINT @MensajeHoras
-	--Print @MensajeHoras2
-
-	declare @titulo varchar(max)
-	Select @titulo = 'Notificacion Reservacion'
-	declare @mensaje varchar(max)
-	SET @mensaje = @MensajeReservacion + @MensajeHoras
-	
-	EXEC msdb.dbo.sp_send_dbmail
-		@profile_Name = 'ProyectoEmail',
-		--@recipients = @copy_to,
-		@recipients = @Email,
-		@subject = @titulo,
-		@body = @Mensaje
-		--@body_format = 'HTML'
-		
-
-end 
-
-go
 
 Exec NotificarReservacion 1
 
@@ -442,7 +443,7 @@ insert into HorasSolicitudTable(dia, horaInicio, horaFinal,numAula,numLab) value
 
 Exec insertarTotal '05-03-2019', 'Andres Gutierrez', 'nombreEmpresa', '2-0004-3123', 'andreguti333@gmail.com', '5124-1351','NombreActividad','12-14-19', '12-20-19','observacion', 12, 123
 
-
+select * from HorasSolicitudTable
 insert into HorasSolicitudTable(dia, horaInicio, horaFinal,numAula,numLab) values ('05-12-19','09:00:00','11:00:00',3,null)
 insert into HorasSolicitudTable(dia, horaInicio, horaFinal,numAula,numLab) values ('05-12-19','13:00:00','15:00:00',1,null)
 insert into HorasSolicitudTable(dia, horaInicio, horaFinal,numAula,numLab) values ('05-12-19','16:00:00','19:00:00',2,null)
